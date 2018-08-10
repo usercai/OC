86199984921dcc1fb3039844bc149bae0d1a6fe9
usercai <tenghongcai@163.com>
2018-08-10 09:17:42 +0800

    Avplayer

---
+++ Demo/AVPlayerDemo/AVPlayerDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AVPlayerDemo
+//
+//  Created by mac on 2018/8/10.
+//  Copyright © 2018年 thc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
